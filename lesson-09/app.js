3ca6cbbbfa3214ed6789e97d266d68c9d4cc4d38
tkyen <tkyen1110@gmail.com>
2020-07-10 13:29:55 +0800

    Revise lesson-09

--- lesson-09/app.js
+++ lesson-09/app.js
@@ -5,16 +5,16 @@ new Vue({
         b: 0,
         age: 20
     },
-    methods: {
-        /*addToA: function(){
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-            console.log('addToB');
-            return this.b + this.age;
-        }*/
-    },
+    // methods: {
+    //     addToA: function(){
+    //         console.log('addToA');
+    //         return this.a + this.age;
+    //     },
+    //     addToB: function(){
+    //         console.log('addToB');
+    //         return this.b + this.age;
+    //     }
+    // },
     computed: {
         addToA: function(){
             console.log('addToA');
