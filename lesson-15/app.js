653c88be08356adf1211e1db3383a538e5540b4c
tkyen <tkyen1110@gmail.com>
2020-07-10 16:39:24 +0800

    Revise lesson-15

--- lesson-15/app.js
+++ lesson-15/app.js
@@ -1,11 +1,25 @@
 Vue.component('greeting', {
-    template: '<p>Hey there, I am a re-usable component</p>'
+    // template: '<p>Hey there, I am a re-usable component</p>',
+    template: ` <div>
+                    <p>Hey there, I am a re-usable component</p>
+                    <p>Hey there, I am {{ name }} <button v-on:click="changeName">Change name</button></p>
+                </div>`,
+    data: function() {
+        return {
+            name: 'Yoshi'
+        }
+    },
+    methods: {
+        changeName: function() {
+            this.name = 'Mario'
+        }
+    }
 });
 
-/* new Vue({
-    el: '.test',
-    template: '<p>I am a template</p>'
-}); */
+// new Vue({
+//     el: '.test',
+//     template: '<p>I am a template</p>'
+// });
 
 new Vue({
     el: '#vue-app-one'
