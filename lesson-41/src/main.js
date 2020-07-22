279579e3e957cd1048f453dbadcf209a2c4b20e3
Tk.Yen <tkyen1110@gmail.com>
2020-07-22 09:01:02 +0800

    Revise lesson-40 and lesson-41

--- lesson-41/src/main.js
+++ lesson-41/src/main.js
@@ -9,6 +9,13 @@ Vue.use(VueResource);
 Vue.use(VueRouter);
 
 // Register routes
+// https://router.vuejs.org/guide/essentials/history-mode.html
+// (mode: 'history') localhost:8081/whatever makes a request to the server.
+// A server is going to look at that request and decide what to do with it and return the appropriate result.
+// (mode: 'hash')    localhost:8081/#whatever does not make an additional request to the server.
+// It just takes me to a different part of the page.
+// You may have seen on websites when you click on a button and it drags you further down to the bottom of the page or vice versa.
+// What they are doing is they are targeting an ID on the page.
 const router = new VueRouter({
     routes: Routes,
     mode: 'history'
